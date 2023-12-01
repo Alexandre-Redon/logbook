e745660694ff9474d7f1a25e0df425fc592d1aeb
WasthElse2002 <alexandreredon.pro@gmail.com>
2023-12-01 11:07:16 +0100

    init: project

---
+++ src/provider/AuthProvider.tsx
@@ -0,0 +1,18 @@
+const defaultValueType = {
+  state: initState,
+  dispatch: () => null,
+};
+
+export const AuthContext = createContext(defaultValueType);
+
+const AuthProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(authReducer, initState);
+
+  return (
+    <AuthContext.Provider value={{ state, dispatch }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export default AuthProvider;
